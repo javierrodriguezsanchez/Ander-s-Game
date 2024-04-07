70d7e9ff9eb5c6b5efa876bece17c2e673d8afc8
alejocp00 <alex00cu@gmail.com>
2024-04-07 12:32:04 -0400

    Created config class

---
+++ config_class.py
@@ -0,0 +1,27 @@
+from reigns import Kingdom
+from Agent_Player import Agent
+
+
+class Config:
+
+    def __init__(
+        self,
+        simulation_kingdoms: list[Kingdom],
+        players: list[Agent],
+        iterations: int = 100,
+    ):
+        self._kingdoms = simulation_kingdoms
+        self._players = players
+        self._iterations = iterations
+
+    @property
+    def kingdoms(self):
+        return [Kingdom(kingdom) for kingdom in self._kingdoms]
+
+    @property
+    def players(self):
+        return [player.clone() for player in self._players]
+
+    @property
+    def iterations(self):
+        return self._iterations
