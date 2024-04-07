333b164b070c871fa4f536951af6ce455e4b3931
alejocp00 <alex00cu@gmail.com>
2024-04-07 12:59:16 -0400

    Created clone method in agent

--- Game.py
+++ Game.py
@@ -2,26 +2,27 @@ from reigns import *
 from Human_Player import *
 import os
 
+
 class Game:
 
-    def __init__(self, Kingdoms: list, Players: list):
+    def __init__(self, Kingdoms: list[Kingdom], Players: list):
         self.kingdoms = Kingdoms
         self.players = Players
 
-    def Run_Game(self):
+    def run_game(self):
         Current_Turn = 0
         while True:
             os.system("cls")
-            self.kingdoms[Current_Turn].NewTurn()
+            self.kingdoms[Current_Turn].new_turn()
             Moves = self.players[Current_Turn].Play(self.kingdoms)
 
             for i in Moves:
-                if (i[0] == "Attack King"):
+                if i[0] == "Attack King":
                     self.players.pop(i[0][2])
                     self.kingdoms.pop(i[0][2])
 
             Current_Turn += 1
-            if (Current_Turn >= len(self.players)):
+            if Current_Turn >= len(self.players):
                 Current_Turn = 0
 
             if len(self.players) == 1:
