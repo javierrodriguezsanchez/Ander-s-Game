03262c1cf0d1b459b735bd0728e5979304153ba9
alejocp00 <alex00cu@gmail.com>
2024-04-08 22:31:31 -0400

    Refactor SimulationInterface class to use LogManager class and add game printing functionality

--- simulation_interface.py
+++ simulation_interface.py
@@ -2,6 +2,7 @@ import os
 from config_class import Config
 from history_process import HistoryProcess
 from llm_interface import LLMInterface
+from log_manager import LogManager
 from simulation import Simulation
 
 
@@ -10,26 +11,46 @@ class SimulationInterface:
 
     def __init__(self) -> None:
         """Initialize the simulation interface."""
-        self._history_process = HistoryProcess()
-        self._llm_interface = LLMInterface()
+        self._log_manager = LogManager()
+        self._history_process = HistoryProcess(self._log_manager)
         self._simulation_config = None
         self._simulation = None
 
-    def get_information(self):
+    def _get_information(self):
         """Gather the necessary values to start the simulation"""
         raise NotImplementedError(
             "This method should be implemented by the child class."
         )
 
-    def set_configuration(self):
+    def _get_game_to_print_index(self):
+        """Get the index of the game to print. The index needs to be between 0 and the number of iterations. For better performance, its recomendable to chose the first one"""
+        raise NotImplementedError(
+            "This method should be implemented by the child class."
+        )
+
+    def _set_simulation(self):
         """Set the configuration for the simulation."""
-        self._simulation = Simulation(self._simulation_config)
+        self._simulation = Simulation(self._simulation_config, self._log_manager)
+
+    def run(self):
+        """Run the simulation"""
+
+        # Get data from user
+        self._get_information()
+        self._set_simulation()
+        self._get_game_to_print_index()
+
+        # Run the simulator
+        self._simulation.run()
+
+        # Start the history process
+        self._history_process.start_process()
 
 
 class SimulationInterfaceConsole(SimulationInterface):
     """This class will be responsible for handling the simulation interface through the console."""
 
-    def get_information(self):
+    def _get_information(self):
         """Gather the necessary values to start the simulation"""
         kingdoms = self._get_amount_of_kingdoms()
         players = kingdoms
@@ -37,9 +58,9 @@ class SimulationInterfaceConsole(SimulationInterface):
 
         self._simulation_config = Config(kingdoms, players, iterations)
 
-    def set_configuration(self):
+    def _set_simulation(self):
         """Set the configuration for the simulation."""
-        super().set_configuration()
+        super()._set_simulation()
 
     def _get_amount_of_kingdoms(self):
         """Ask the user how many kingdoms will use"""
@@ -80,3 +101,27 @@ class SimulationInterfaceConsole(SimulationInterface):
             iterations = input(promp)
 
         return int(iterations)
+
+    def _get_game_to_print_index(self):
+        """Get the index of the game to print. The index needs to be between 0 and the number of iterations. For better perfomance, its recomendable to chose the first one"""
+        # Clear the console
+        self._clear_console()
+
+        # Ask for the index
+        promp = "Which game do you want to print? (0 - N)"
+        index = input(promp)
+
+        # Validate input
+        while (
+            not index.isdigit()
+            or int(index) < 0
+            or int(index) >= self._simulation_config.number_of_simulations
+        ):
+            self._clear_console()
+            print(
+                "Please, enter a valid number for the index. Remember that it needs to be between 0 and N."
+            )
+            index = input(promp)
+
+        # Set the game to print
+        self._simulation.change_game_to_print(int(index))
