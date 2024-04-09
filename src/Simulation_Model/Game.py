9b8c7c976a271a0c887f2bbb62295a1ac4adcaa3
alejocp00 <alex00cu@gmail.com>
2024-04-09 11:05:19 -0400

    Refactor Game class to improve readability and add max_rounds parameter in __init__ method

--- src/Simulation_Model/Game.py
+++ src/Simulation_Model/Game.py
@@ -1,28 +1,29 @@
+from Agent.Agent import Agent
 from src.Simulation_Model.Reigns import Kingdom
 import os
 
 
 class Game:
 
-    def __init__(self, Kingdoms: list[Kingdom], Players: list):
+    def __init__(
+        self, Kingdoms: list[Kingdom], Players: list[Agent], max_rounds: int = 100
+    ):
         self.kingdoms = Kingdoms
         self.players = Players
+        self._max_rounds = max_rounds
 
     def run_game(self):
-        Current_Turn = 0
-        while True:
-            os.system("cls")
-            self.kingdoms[Current_Turn].new_turn()
-            Moves = self.players[Current_Turn].Play(self.kingdoms)
+        current_turn = 0
+        current_round = 0
 
-            for i in Moves:
-                if i[0] == "Attack King":
-                    self.players.pop(i[0][2])
-                    self.kingdoms.pop(i[0][2])
+        players_count = len(self.players)
 
-            Current_Turn += 1
-            if Current_Turn >= len(self.players):
-                Current_Turn = 0
-
-            if len(self.players) == 1:
-                break
+        while players_count > 1 or current_round < self._max_rounds:
+            for i in range(players_count):
+                self.players[i].Update_State(self.kingdoms, i)
+                self.players[i].Number_Of_Players(players_count)
+                self.players[i].Play()
+                self.players[i].EndTurn()
+                current_turn += 1
+                if current_turn % players_count == 0:
+                    current_round += 1
