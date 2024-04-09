3a6b58a3aa866c772a6dc97028214a0445d85000
Dráckaro <luisalejandrorodriguezotero@gmail.com>
2024-04-09 17:29:52 -0400

    Refactorizing stuff on strategies

--- src/Strategies/Implementations/AttackWeakStrategy.py
+++ src/Strategies/Implementations/AttackWeakStrategy.py
@@ -1,10 +1,15 @@
 from Simulation_Model.Reigns import Kingdom
 from src.Strategies.Strategy import Strategy
-from src.Strategies.Implementations.utils.utils import Media
+from src.Strategies.Implementations.utils.utils import Media, Bad_Ending_for_i, Strongest_Army, Weakest_Army
 import random
 
 
 class AttackWeakStrategy(Strategy):
+    '''
+    Attacks the kingdom with the weakest army.
+
+    Offer/Accept alliance only with the kingdom of the strongest army.
+    '''
     def __init__(self):
         pass
 
@@ -16,66 +21,10 @@ class AttackWeakStrategy(Strategy):
         Allies: list[int],
     ) -> int:
         
-        less_troops = -1
-        troops = -1
-        troop_media = -1
         current_state=posible_actions[0]
-        for i in range(len(current_state)):
-            if i == my_index:
-                continue
-
-            current_troops = current_state[i].army
-            if len(current_troops) < troops or troops == -1:
-                troops = len(current_troops)
-                less_troops = i
-                troop_media = Media(current_troops)
-
-            elif len(current_troops) == troops:
-                media = Media(current_troops)
-
-                if media < troop_media:
-                    troop_media = media
-                    less_troops = i
-
-        best_end = -1
-        min_wall = current_state[less_troops].walls
-        min_media = Media(current_state[less_troops].army)
-        min_len = len(current_state[less_troops].army)
-        min_pop = current_state[less_troops].population
-
-        for i, state in enumerate(posible_actions):
-            current_army = state[less_troops].army
-            current_len = len(current_army)
-            current_media = Media(current_army)
-            current_wall = state[less_troops].walls
-            current_pop = state[less_troops].population
-
-            if current_len < min_len:
-                min_len = current_len
-                min_media = current_media
-                min_wall = current_wall
-                min_pop = current_pop
-                best_end = i
-
-            elif current_len == min_len:
-                if current_media < min_media:
-                    min_media = current_media
-                    min_wall = current_wall
-                    min_pop = current_pop
-                    best_end = i
-
-            if current_len == 0 and min_len == 0:
-                if current_wall < min_wall:
-                    min_wall = current_wall
-                    min_pop = current_pop
-                    best_end = i
-
-                elif current_wall == min_wall:
-                    if current_pop <= min_pop:
-                        min_pop = current_pop
-                        best_end = i
-
-        return best_end
+        less_troops = Weakest_Army(current_state, my_index)
+        best_ending = Bad_Ending_for_i(posible_actions, less_troops)
+        return best_ending
 
     def ChooseAllies(
         self,
@@ -85,26 +34,7 @@ class AttackWeakStrategy(Strategy):
         Allies: list[int],
     ) -> list[bool]:
         
-        more_troops = -1
-        troops = -1
-        troop_media = -1
-
-        for i in range(len(Kingdoms)):
-            if i == my_index:
-                continue
-
-            current_troops = Kingdoms[i].army
-            if len(current_troops) > troops:
-                troops = len(current_troops)
-                more_troops = i
-                troop_media = Media(current_troops)
-
-            elif len(current_troops) == troops:
-                media = Media(current_troops)
-
-                if media > troop_media:
-                    troop_media = media
-                    more_troops = i
+        more_troops = Strongest_Army(Kingdom, my_index)
 
         prop = [False] * len(Kingdoms)
         if Allies[more_troops] > 0:
@@ -125,26 +55,7 @@ class AttackWeakStrategy(Strategy):
         Allies: list[int],
     ) -> bool:
         
-        more_troops = -1
-        troops = -1
-        troop_media = -1
-
-        for i in range(len(Kingdoms)):
-            if i == my_index:
-                continue
-
-            current_troops = Kingdoms[i].army
-            if len(current_troops) > troops:
-                troops = len(current_troops)
-                more_troops = i
-                troop_media = Media(current_troops)
-
-            elif len(current_troops) == troops:
-                media = Media(current_troops)
-
-                if media > troop_media:
-                    troop_media = media
-                    more_troops = i
+        more_troops = Strongest_Army(Kingdom ,my_index)
 
         if prop_index == more_troops:
             return True
