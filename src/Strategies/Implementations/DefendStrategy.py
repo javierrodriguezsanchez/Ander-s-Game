52c4e90ea5b5276ac976b53c94c2cfbb08f4d0e4
Dráckaro <luisalejandrorodriguezotero@gmail.com>
2024-04-09 18:25:44 -0400

    Implemented Defend Strategy

---
+++ src/Strategies/Implementations/DefendStrategy.py
@@ -0,0 +1,65 @@
+from Simulation_Model.Reigns import Kingdom
+from src.Strategies.Strategy import Strategy
+from src.Strategies.Implementations.utils.utils import Media, Bad_Ending_for_i, Defensive_Ending_For_i, Strongest_Army, Weakest_Army
+import random
+
+class DefendStrategy(Strategy):
+    '''
+    Upgrades his wall and army for defend himself.
+
+    Attacks with a low probability a kingdom with a very low relation.
+
+    Do not offer alliance and only accept propotions from non enemy (reel > 0) kingdoms
+    '''
+
+    def Select(
+        self,
+        my_index: int,
+        posible_actions: list[list[Kingdom]],
+        reels: list[int] = [],
+        Allies: list[int] = [],
+    ) -> int:
+        
+        enemy = -1
+
+        for i in range(len(reels)):
+            if reels[i] <= -50:
+                enemy = i
+
+        rand = random.random()
+        if rand < 0.2:
+            best_ending = Bad_Ending_for_i(posible_actions, enemy)
+            return best_ending
+        
+        best_ending = Defensive_Ending_For_i(posible_actions, my_index)
+        return best_ending
+    
+    def ChooseAllies(
+        self,
+        Kingdoms: list[Kingdom],
+        my_index: int,
+        reels: list[int],
+        Allies: list[int],
+    ) -> list[bool]:
+        
+        return [False] * len(Kingdoms)
+    
+    def AcceptAlliance(
+        self,
+        Kingdoms: list[Kingdom],
+        my_index: int,
+        prop_index: int,
+        reels: list[int],
+        Allies: list[int],
+    ) -> bool:
+        
+        if reels[prop_index] >= 10:
+            return True
+        elif reels[prop_index] > 0:
+            rand = random.random()
+            if rand < 0.5:
+                return True
+            else:
+                return False
+        else:
+            return False
--- src/Strategies/Implementations/utils/utils.py
+++ src/Strategies/Implementations/utils/utils.py
@@ -1,4 +1,5 @@
 from src.Simulation_Model.Reigns import Kingdom
+import random
 
 def Media(colection):
     sum_ = sum(colection)
@@ -7,7 +8,7 @@ def Media(colection):
 
 def Bad_Ending_for_i(posible_actions: list[list[Kingdom]], i: int)-> int:
     '''
-    Return the index of the worst ending for the kingdom i
+    Returns the index of the worst ending for the kingdom i
     '''
     current_state=posible_actions[0]
 
@@ -17,43 +18,90 @@ def Bad_Ending_for_i(posible_actions: list[list[Kingdom]], i: int)-> int:
     min_len = len(current_state[i].army)
     min_pop = current_state[i].population
 
-    for i, state in enumerate(posible_actions):
+    for j, state in enumerate(posible_actions):
         current_army = state[i].army
         current_len = len(current_army)
         current_media = Media(current_army)
         current_wall = state[i].walls
         current_pop = state[i].population
 
+        if not state[i].king_alive:
+            best_end = j
+            break
+
         if current_len < min_len:
             min_len = current_len
             min_media = current_media
             min_wall = current_wall
             min_pop = current_pop
-            best_end = i
+            best_end = j
 
         elif current_len == min_len:
             if current_media < min_media:
                 min_media = current_media
                 min_wall = current_wall
                 min_pop = current_pop
-                best_end = i
+                best_end = j
 
         if current_len == 0 and min_len == 0:
             if current_wall < min_wall:
                 min_wall = current_wall
                 min_pop = current_pop
-                best_end = i
+                best_end = j
 
             elif current_wall == min_wall:
                 if current_pop <= min_pop:
                     min_pop = current_pop
-                    best_end = i
+                    best_end = j
 
         return best_end
     
+def Defensive_Ending_For_i(posible_actions: list[list[Kingdom]], i: int)-> int:
+    '''
+    Returns the index of the best defensive final for the kingdom i.
+    '''
+
+    initial_wall = posible_actions[0][i].walls
+    greater_wall = -1
+    greater_wall_state = -1
+    greater_media = -1
+    greater_media_state = -1
+    greater_media_of_valid_states = -1
+    best_ending = -1
+
+    for j, state in enumerate(posible_actions):
+        current_wall = state[i].walls
+        current_army = state[i].army
+        current_len = len(current_army)
+        current_media = Media(current_army)
+        current_min = min(current_army)
+
+        if current_wall > greater_wall:
+            greater_wall = current_wall
+            greater_wall_state = j
+
+        if current_media > greater_media:
+            greater_media = current_media
+            greater_media_state = j
+
+        if current_wall - initial_wall >= 2 and current_len >= 3 and current_min >= 3:
+            if current_media > greater_media_of_valid_states:
+                greater_media_of_valid_states = current_media
+                best_ending = j
+
+    if best_ending != -1:
+        return best_ending
+    else:
+        rand = random.random()
+        if rand < 0.5:
+            return greater_wall_state
+        else:
+            return greater_media_state
+
+    
 def Strongest_Army(Kingdoms: list[Kingdom], my_index: int) -> int:
     '''
-    Return the index of the kingdom with the strongest army (if my_index = -1 includes all the kingdoms in the comparison)
+    Returns the index of the kingdom with the strongest army (if my_index = -1 includes all the kingdoms in the comparison)
     '''
 
     more_troops = -1
@@ -61,7 +109,7 @@ def Strongest_Army(Kingdoms: list[Kingdom], my_index: int) -> int:
     troop_media = -1
 
     for i in range(len(Kingdoms)):
-        if i == my_index:
+        if i == my_index or not Kingdoms[i].king_alive:
             continue
 
         current_troops = Kingdoms[i].army
@@ -89,7 +137,7 @@ def Weakest_Army(Kingdoms: list[Kingdom], my_index: int) -> int:
     troop_media = -1
     
     for i in range(len(Kingdoms)):
-        if i == my_index:
+        if i == my_index or not Kingdoms[i].king_alive:
             continue
 
         current_troops = Kingdoms[i].army
